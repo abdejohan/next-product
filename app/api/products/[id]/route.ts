a7d3b7ecd8a125b6e9181e1ee8ce539353aeb865
Izabela Marcinkowska <izabela.marcinkowska@appliedtechnology.se>
2023-12-13 15:56:40 +0100

    work in progress: Fixing zod

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -1,11 +1,20 @@
-import { getProduct } from '@/utils';
-import { PrismaClient } from '@prisma/client';
+import { getProduct } from "@/utils";
+import { PrismaClient } from "@prisma/client";
+import { notFound } from "next/navigation";
+import { z } from "zod";
+
+const mySchema = z.string().min(5);
 
 export async function GET(
   request: Request,
   { params }: { params: { id: string } }
 ) {
   try {
+    const zoded = mySchema.safeParse(params.id);
+    if (!zoded.success) {
+      console.log("That didnt work out");
+      notFound();
+    }
     const prisma = new PrismaClient();
     const product = await prisma.product.findUnique({
       where: { id: params.id },
--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,23 +1,24 @@
-import { getProduct } from '@/utils';
-import Image from 'next/image';
+import { getProduct } from "@/utils";
+import Image from "next/image";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 const ProductPage = async ({ params: { id } }: { params: { id: string } }) => {
   const product = (await getProduct(id)) as any;
   const response = await fetch(`http://localhost:3000/api/products/${id}`);
   const data = await response.json();
-  console.log('data', data);
+  console.log("data", data);
 
   return (
     <>
       <h1>this is one single product page</h1>
       <h2>{product.fields.title}</h2>
       <h2>{product.fields.description}</h2>
-      <h2>{product.fields.price}</h2>
+      <h2>Price: {product.fields.price}</h2>
+      <h2>Quantity: {data.quantity}</h2>
       <Image
         alt="product image"
-        src={'https:' + product.fields.productImage.fields.file.url}
+        src={"https:" + product.fields.productImage.fields.file.url}
         width={500}
         height={500}
       />
